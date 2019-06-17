ec211bf62ef983e34abb217c528643b512a38dd7
AhmedRaafat2 <chemist.ahmedraafat@gmail.com>
2019-06-17 07:29:53 +0200

    2nd modify

--- app/src/main/java/com/example/quizapp/ExamActivity.java
+++ app/src/main/java/com/example/quizapp/ExamActivity.java
@@ -1,15 +1,16 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import java.util.concurrent.TimeUnit;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,7 +23,16 @@ public class ExamActivity extends AppCompatActivity implements View.OnClickListe
    @BindView(R.id.q2_answers_group) RadioGroup questionTwoeGroup;
    @BindView(R.id.q3_answers_group) RadioGroup questionThreeGroup;
    @BindView(R.id.q4_answers_group) RadioGroup questionFourGroup;
-   private int studentScore;
+   @BindView(R.id.q5_a1_btn) CheckBox questionFiveA1;
+   @BindView(R.id.q5_a2_btn) CheckBox questionFiveA2;
+   @BindView(R.id.q5_a3_btn) CheckBox questionFiveA3;
+   @BindView(R.id.q5_a4_btn) CheckBox questionFiveA4;
+   @BindView(R.id.q6_a1_btn) CheckBox questionSixA1;
+   @BindView(R.id.q6_a2_btn) CheckBox questionSixA2;
+   @BindView(R.id.q6_a3_btn) CheckBox questionSixA3;
+   @BindView(R.id.q6_a4_btn) CheckBox questionSixA4;
+   @BindView(R.id.q7_answer) EditText questionSevenAnswer_et;
+   private int studentScore =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +76,31 @@ public class ExamActivity extends AppCompatActivity implements View.OnClickListe
         if(questionFourGroup.getCheckedRadioButtonId() == R.id.q4_a1_btn){
             studentScore++;
         }
-        Intent resultIntent = new Intent(this,ResultActivity.class);
-        resultIntent.putExtra("studentName",getIntent().getExtras().getString("studentName"));
-        resultIntent.putExtra("studentID",getIntent().getExtras().getString("studentID"));
-        resultIntent.putExtra("studentScore",studentScore);
-        startActivity(resultIntent);
+        if(questionFiveA2.isChecked()){
+            studentScore++;
+        }
+        if(questionSixA3.isChecked()){
+            studentScore++;
+        }
+        if(questionSevenAnswer_et.getText().toString().matches("no")){
+            studentScore++;
+        }
+
+        int percentage = (Math.multiplyExact(studentScore, 100) / 7);
+
+        if (percentage >= 50) {
+            Toast.makeText(this,getString(R.string.studentName) + " " + getIntent().getStringExtra("studentName")+"\n"+
+                    getString(R.string.studentID) + " " + getIntent().getStringExtra("studentID")+"\n"+
+                    getString(R.string.studentScore_part1) + " " + studentScore + " " + getString(R.string.studentScore_part2)+"\n"+
+                            getString(R.string.studentPercentage) + " " + percentage + "%"+"\n"+
+                    getString(R.string.studentState_part1) + " " + getString(R.string.studentPass),Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this,getString(R.string.studentName) + " " + getIntent().getStringExtra("studentName")+"\n"+
+                    getString(R.string.studentID) + " " + getIntent().getStringExtra("studentID")+"\n"+
+                    getString(R.string.studentScore_part1) + " " + studentScore + " " + getString(R.string.studentScore_part2)+"\n"+
+                    getString(R.string.studentPercentage) + " " + percentage + "%"+"\n"+
+                    getString(R.string.studentState_part1) + " " + getString(R.string.studentFail),Toast.LENGTH_LONG).show();
+        }
         finish();
     }
 }
